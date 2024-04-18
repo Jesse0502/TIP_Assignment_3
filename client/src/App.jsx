83ee65e086f40ea8fdcebe270722e43eade4da0a
John Doe <jas0502singh@gmail.com>
2024-04-18 15:06:04 +1000

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Box } from '@chakra-ui/react'
+function App() {
+
+  return (
+    <Box>
+      <p>
+        Hello world
+      </p>
+    </Box>
+  )
+}
+
+export default App
